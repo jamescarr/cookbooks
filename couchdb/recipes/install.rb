50cd01677fd30481e746e4d44c7081a3df44809e
jamescarr <jamescarr@abidos.(none)>
2009-05-11 23:38:00 -0500

    added platform check

--- couchdb/recipes/install.rb
+++ couchdb/recipes/install.rb
@@ -1,15 +1,17 @@
 %w{"libmozjs-dev", "libicu-dev", "libcurl4-openssl-dev", "erlang"}.each {|pkg| package(pkg) }
 
-script "install_couchdb" do
-  interpreter "bash"
-  user "root"
-  cwd "/tmp"
-  code <<-EOH
-    wget http://mirrors.24-7-solutions.net/pub/apache/couchdb/0.9.0/apache-couchdb-0.9.0.tar.gz  
-    tar zxvf apache-couchdb-0.9.0.tar.gz  
-    cd apache-couchdb-0.9.0  
-    ./configure && make && sudo make install
-  EOH
-end
-  
+case platform
+when "debian", "ubuntu"
+  script "install_couchdb" do
+    interpreter "bash"
+    user "root"
+    cwd "/tmp"
+    code <<-EOH
+      wget http://mirrors.24-7-solutions.net/pub/apache/couchdb/0.9.0/apache-couchdb-0.9.0.tar.gz  
+      tar zxvf apache-couchdb-0.9.0.tar.gz  
+      cd apache-couchdb-0.9.0  
+      ./configure && make && sudo make install
+    EOH
+  end
+end  
 
